75c9a156ec94f6c4b4d77922e479b73ccfb79b34
B-atuti <brian.atuti@student.moringaschool.com>
2023-07-29 17:55:05 -0400

    Updates

--- b-atuti/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import App from './components/App';
-import './index.css';
-
-// Use createRoot instead of ReactDOM.render
-createRoot(document.getElementById('root')).render(<App />);
